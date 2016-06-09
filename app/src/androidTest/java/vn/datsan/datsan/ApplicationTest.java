03141120ac150d26de191a2c9f5971a0d3a08887
Yen Nguyen <yen.nguyen@amaris.com>
2016-06-09 15:26:53 +0700

    Adding vcs to the source

--- app/src/androidTest/java/vn/datsan/datsan/ApplicationTest.java
+++ app/src/androidTest/java/vn/datsan/datsan/ApplicationTest.java
@@ -1,7 +1,7 @@
 package vn.datsan.datsan;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+        import android.app.Application;
+        import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
