55ca5ab7a04f5d8b7a9da20f3f224a1de64fba31
XuanPham <xuanpham.cse@gmail.com>
2016-06-19 19:59:02 +0700

    merge code

---
+++ app/src/main/java/vn/datsan/datsan/activities/ProfileActivity.java
@@ -0,0 +1,33 @@
+package vn.datsan.datsan.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import vn.datsan.datsan.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
