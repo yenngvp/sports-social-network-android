7712af157bcd9c0d296f1ee7f1f42f74377fecc3
XuanPham <xuanpham.cse@gmail.com>
2016-08-05 21:39:49 +0700

    search integration

---
+++ app/src/main/java/vn/datsan/datsan/search/AppSearch.java
@@ -0,0 +1,45 @@
+package vn.datsan.datsan.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.searchbox.core.SearchResult;
+import vn.datsan.datsan.models.Field;
+import vn.datsan.datsan.serverdata.CallBack;
+import vn.datsan.datsan.utils.AppLog;
+
+/**
+ * Created by xuanpham on 8/5/16.
+ */
+public class AppSearch {
+
+    public static void searchField(String keyword, String distance, float lati, float longi, CallBack.OnSearchResultListener callback) {
+        keyword = "san bong da";
+        List<String> searchTypes = new ArrayList<>();
+        searchTypes.add(Field.class.getSimpleName());
+        SearchOption searchOption = new SearchOption(keyword, searchTypes);
+        searchOption.setFilteredDistance("1km");
+        searchOption.setLatLon("10.77533", "106.69453");
+//        ElasticsearchService.getInstance().search(searchOption, callback);
+        ElasticsearchService.getInstance().search(searchOption, new CallBack.OnSearchResultListener() {
+            @Override
+            public void onSearchResult(SearchResult searchResult) {
+                if (searchResult == null) {
+                    // No search result found
+                    return;
+                }
+
+                AppLog.log(AppLog.LogType.LOG_INFO, "TAG", "Search result callback returns: " + searchResult.getTotal());
+
+                // Get search result type fields
+                List<SearchResult.Hit<Field, Void>> fieldHits = searchResult.getHits(Field.class);
+                for (SearchResult.Hit hit : fieldHits) {
+                    String type = hit.type;
+                    Object source = hit.source;
+
+                    AppLog.log(AppLog.LogType.LOG_INFO, "TAG", "Found a " + type + " : " + source);
+                }
+            }
+        });
+    }
+}
